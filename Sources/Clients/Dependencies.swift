90053851d17c913fa7564b2bc0576e3d37969ea3
Dominic Ndondo <59946287+Dominic263@users.noreply.github.com>
2023-09-23 02:43:31 -0400

    finished updates on connect endpoint of the MQTT dependency

--- Sources/Clients/Dependencies.swift
+++ Sources/Clients/Dependencies.swift
@@ -12,34 +12,11 @@ import NIOTransportServices
 import ComposableArchitecture
 
 
-
-/*
- func connect() async {
-     do {
-         _ = try await self.client.connect(cleanSession: view.serverDetails.cleanSession)
-         self.client.addCloseListener(named: "EmCuTeeTee") { result in
-             guard !self.shuttingDown else { return }
-             Task {
-                 await self.view.addMessage("Connection closed", now: true)
-                 await self.view.addMessage("Reconnecting...", now: true)
-                 await self.connect()
-             }
-         }
-         await self.view.addMessage("Connection successful", now: true)
-     } catch {
-         await self.view.addMessage("Failed to connect\n\(error)", now: true)
-     }
- }
- 
- */
 ///MQTTDependencies controls the MQTT client
 public struct MQTTDependencies {
     public static var client: MQTTClient? = nil
     public var connect: (ServerDetails) async -> Bool
 }
-
-
-//static let eventLoopGroup = NIOTSEventLoopGroup()
 
 extension MQTTClient {
     
@@ -70,6 +47,7 @@ extension MQTTDependencies: DependencyKey {
                  _  = try await MQTTDependencies.client?.connect(cleanSession: true)
                  //try to secure a connection
              Task {
+                 //TODO: Check to see if this line is needed at all.
                  MQTTDependencies.client?.addCloseListener(named: "dndond01") { result in
                      //guard !self.shuttingDown else { return }
                  }
